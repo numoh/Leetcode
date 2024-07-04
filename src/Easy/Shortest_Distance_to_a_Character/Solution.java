b1a6c4f1b129cbaf167be659471137ec2d0d6a29
Nuh <numoh.55@gmail.com>
2024-07-04 06:54:01 +0200

    Solved problem 821

--- src/Easy/Shortest_Distance_to_a_Character/Solution.java
+++ src/Easy/Shortest_Distance_to_a_Character/Solution.java
@@ -1,5 +1,6 @@
 package Easy.Shortest_Distance_to_a_Character;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 // 821
@@ -11,6 +12,34 @@ public class Solution {
     }
 
     public static int[] shortestToChar(String s, char c) {
-        return null;
+        ArrayList<Integer> characterC = new ArrayList();
+        int[] distanceBetweenCharacters = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                characterC.add(i);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int ans = shortestDistance(i, characterC);
+            distanceBetweenCharacters[i] = ans;
+        }
+
+        return distanceBetweenCharacters;
+    }
+
+    private static int shortestDistance(int i, ArrayList<Integer> characterC) {
+        int res = 0;
+        res = Math.abs(characterC.get(0) - i);
+
+        for (int j = 1; j < characterC.size(); j++) {
+            int shortestDistance = Math.abs(characterC.get(j) - i);
+            if (shortestDistance < res) {
+                res = shortestDistance;
+            }
+        }
+
+        return res;
     }
 }
