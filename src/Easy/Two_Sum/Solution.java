7a361252c6fb1acc52b3e713c560e457995d8b94
Nuh <numoh.55@gmail.com>
2024-07-10 19:52:20 +0200

    Solved problem 1

---
+++ src/Easy/Two_Sum/Solution.java
@@ -0,0 +1,25 @@
+package Easy.Two_Sum;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i , j};
+                }
+            }
+        }
+
+        return null;
+    }
+}
