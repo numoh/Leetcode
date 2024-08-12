456e23a063ea8cb9caecf4e7299832e5a3ecac64
Nuh Jama <numoh.55@gmail.com>
2024-08-12 20:02:11 +0200

    Solved problem 412

---
+++ src/Easy/Fizz_Buzz/Solution.java
@@ -0,0 +1,30 @@
+package Easy.Fizz_Buzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3));
+        System.out.println(fizzBuzz(5));
+        System.out.println(fizzBuzz(15));
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        ArrayList<String> res = new ArrayList<>();
+
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 15 == 0) {
+                res.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                res.add("Fizz");
+            } else if (i % 5 == 0) {
+                res.add("Buzz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+
+        return res;
+    }
+}
