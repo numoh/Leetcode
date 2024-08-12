e0cc1cc6893f23b506e4f84f054fabf5e5945505
Nuh Jama <numoh.55@gmail.com>
2024-08-12 04:26:48 +0200

    Better solution for problem 290

--- src/Easy/Word_Pattern/Solution.java
+++ src/Easy/Word_Pattern/Solution.java
@@ -10,31 +10,64 @@ public class Solution {
         System.out.println(wordPattern("aaaa", "dog cat cat dog"));
     }
 
+    // My solution
+//    public static boolean wordPattern(String pattern, String s) {
+//        HashMap<String, Integer> hashMapForPattern = new HashMap<>();
+//        HashMap<String, Integer> hashMapForS = new HashMap<>();
+//        StringBuilder stringBuilderForPattern = new StringBuilder();
+//        StringBuilder stringBuilderForS = new StringBuilder();
+//        String[] sSplit = s.split(" ");
+//
+//        for (int i = 0; i < pattern.length(); i++) {
+//            if (!hashMapForPattern.containsKey(String.valueOf(pattern.charAt(i)))) {
+//                hashMapForPattern.put(String.valueOf(pattern.charAt(i)), i);
+//                stringBuilderForPattern.append(hashMapForPattern.get(String.valueOf(pattern.charAt(i))) + 1);
+//            } else {
+//                stringBuilderForPattern.append(hashMapForPattern.get(String.valueOf(pattern.charAt(i))) + 1);
+//            }
+//        }
+//
+//        for (int i = 0; i < sSplit.length; i++) {
+//            if (!hashMapForS.containsKey(sSplit[i])) {
+//                hashMapForS.put(sSplit[i], i);
+//                stringBuilderForS.append(hashMapForS.get(sSplit[i]) + 1);
+//            } else {
+//                stringBuilderForS.append(hashMapForS.get(sSplit[i]) + 1);
+//            }
+//        }
+//
+//        return stringBuilderForPattern.compareTo(stringBuilderForS) == 0;
+//    }
+
+    // Better solution
+
+    // better solution
     public static boolean wordPattern(String pattern, String s) {
-        HashMap<String, Integer> hashMapForPattern = new HashMap<>();
-        HashMap<String, Integer> hashMapForS = new HashMap<>();
-        StringBuilder stringBuilderForPattern = new StringBuilder();
-        StringBuilder stringBuilderForS = new StringBuilder();
-        String[] sSplit = s.split(" ");
+        String[] words = s.split(" ");
+       
+        if (words.length != pattern.length()) return false;
+        
+        HashMap<Character, String> charToWordMap = new HashMap<>();
+        HashMap<String, Character> wordToCharMap = new HashMap<>();
 
         for (int i = 0; i < pattern.length(); i++) {
-            if (!hashMapForPattern.containsKey(String.valueOf(pattern.charAt(i)))) {
-                hashMapForPattern.put(String.valueOf(pattern.charAt(i)), i);
-                stringBuilderForPattern.append(hashMapForPattern.get(String.valueOf(pattern.charAt(i))) + 1);
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (charToWordMap.containsKey(c)) {
+                if (!charToWordMap.get(c).equals(word)) {
+                    return false;
+                }
             } else {
-                stringBuilderForPattern.append(hashMapForPattern.get(String.valueOf(pattern.charAt(i))) + 1);
+                if (wordToCharMap.containsKey(word)) {
+                    return false;
+                }
+                charToWordMap.put(c, word);
+                wordToCharMap.put(word, c);
             }
         }
 
-        for (int i = 0; i < sSplit.length; i++) {
-            if (!hashMapForS.containsKey(sSplit[i])) {
-                hashMapForS.put(sSplit[i], i);
-                stringBuilderForS.append(hashMapForS.get(sSplit[i]) + 1);
-            } else {
-                stringBuilderForS.append(hashMapForS.get(sSplit[i]) + 1);
-            }
-        }
-
-        return stringBuilderForPattern.compareTo(stringBuilderForS) == 0;
+        return true;
     }
+
 }
